3a34dac0f2620f35784cb61fa0569dbca2a36dfa
luca-schmidt <luca.schmidt@proton.me>
2026-03-19 07:04:56 +0000

    Deploying to gh-pages from @ luca-schmidt/luca-schmidt@a4ff34c414d01a8b3bf79a84c20d4473814fb225 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://luca-schmidt.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://luca-schmidt.github.io/luca-schmidt/#contact">contact us</a>.'
               }
             ]
           }
